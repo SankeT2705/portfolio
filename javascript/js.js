ad450592929b3807e97cb18c1b75ccad4a24cc9c
Sanket Raghunath Bobhate <76879335+SankeT2705@users.noreply.github.com>
2022-12-18 15:10:27 +0530

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -1,9 +1,1 @@
-burger = document.querySelector('.burger')
-navbar = document.querySelector('.navbar')
-rightnav = document.querySelector('.rightnav')
-navlist = document.querySelector('.nav-list')
-burger.addEventListener('click', () => {
-    navlist.classList.toggle('v-nav');
-    rightnav.classList.toggle('v-nav');
-    navbar.classList.toggle('h-nav');
-})
+burger = document.querySelector('.burger');navbar = document.querySelector('.navbar');rightnav = document.querySelector('.rightnav');navlist = document.querySelector('.nav-list');burger.addEventListener('click', () => {navlist.classList.toggle('v-nav');rightnav.classList.toggle('v-nav');navbar.classList.toggle('h-nav');})
